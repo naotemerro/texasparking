ae00f21fa3520ea660eaecc2d1c5843b70828df8
Victor Marques <naotemerro@hotmail.com>
2024-11-18 19:21:09 -0300

    Criado projeto

---
+++ src/main/kotlin/com/texasparking/EstacionamentoApplication.kt
@@ -0,0 +1,11 @@
+package com.texasparking
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EstacionamentoApplication
+
+fun main(args: Array<String>) {
+	runApplication<EstacionamentoApplication>(*args)
+}
